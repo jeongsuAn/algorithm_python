be3162e85fb3f355a549aeaa02e7aa9fb580d581
jeongsuAn <72205694+jeongsuAn@users.noreply.github.com>
2022-07-19 12:20:38 +0900

    Time: 100 ms (15.06%), Space: 13.8 MB (76.17%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,66 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        sLen = len(s)
-        result = 0
-        i = 0
-        while i < sLen:
-            if s[i] == "I":
-                if i+1 != sLen and s[i + 1] == "V":
-                    result += 4
-                    i += 2
-                    continue
-                if i+1 != sLen and s[i + 1] == "X":
-                    result += 9
-                    i += 2
-                    continue
-                result += 1
-                i += 1
-                continue
-
-            if s[i] == "V":
-                result += 5
-                i += 1
-                continue
-
-            if s[i] == "X":
-                if i+1 != sLen and s[i + 1] == "L":
-                    result += 40
-                    i += 2
-                    continue
-                if i+1 != sLen and s[i + 1] == "C":
-                    result += 90
-                    i += 2
-                    continue
-                result += 10
-                i += 1
-                continue
-
-            if s[i] == "L":
-                result += 50
-                i += 1
-                continue
-
-            if s[i] == "C":
-                if i+1 != sLen and s[i + 1] == "D":
-                    result += 400
-                    i += 2
-                    continue
-                if i+1 != sLen and s[i + 1] == "M":
-                    result += 900
-                    i += 2
-                    continue
-                result += 100
-                i += 1
-                continue
-
-            if s[i] == "D":
-                result += 500
-                i += 1
-                continue
-
-            if s[i] == "M":
-                result += 1000
-                i += 1
-                continue
-
-        return result
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number

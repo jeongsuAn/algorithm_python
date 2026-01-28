ec1db890136be7a5224d3395bc6c6db3e6032e16
안정수 <72205694+jeongsuAn@users.noreply.github.com>
2026-01-28 23:35:37 +0900

    Time: 0 ms (100%), Space: 19.3 MB (25.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,8 +4,8 @@ class Solution:
         even_sum = 0
         for i in range(len(nums)):
             if i % 2 == 0:
-                even_sum = max(even_sum + nums[i], odd_sum)
-            else:
                 odd_sum = max(odd_sum + nums[i], even_sum)
+            else:
+                even_sum = max(even_sum + nums[i], odd_sum)
         return max(odd_sum, even_sum)
         
